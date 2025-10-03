4e2039229ac370e9c694d343970c7563398f4faa
iam4pple <4pple@iam4pple.com>
2025-10-03 12:45:38 -0500

    change to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [auth()],
   adapter: cloudflare(),
-  output: 'server',
+  output: 'static',
   image: {
     service: passthroughImageService()
   }
