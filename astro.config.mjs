284be0418d24f7d8991b95ddd611c707e9f680c7
iam4pple <4pple@iam4pple.com>
2025-10-03 12:49:48 -0500

    remove auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
-import auth from 'auth-astro';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth()],
   adapter: cloudflare(),
   output: 'static',
   image: {
