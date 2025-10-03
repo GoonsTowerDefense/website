a421cf2b60c4d8081f3c04c016f1b232e17a85d5
iam4pple <4pple@iam4pple.com>
2025-10-03 01:48:32 -0500

    yeah

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import auth from 'auth-astro';
+
+import cloudflare from '@astrojs/cloudflare';
+
+import db from '@astrojs/db';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [auth(), db()],
+  adapter: cloudflare(),
+  output: 'server',
+});
