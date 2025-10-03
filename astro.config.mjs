c8ca004cd296674645ce3fe9da98c040ec8239f5
iam4pple <4pple@iam4pple.com>
2025-10-03 01:55:05 -0500

    db remove

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,9 @@ import auth from 'auth-astro';
 
 import cloudflare from '@astrojs/cloudflare';
 
-import db from '@astrojs/db';
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(), db()],
+  integrations: [auth()],
   adapter: cloudflare(),
   output: 'server',
 });
